96b4e8363a9b31f3ccec94584b0203a181687a33
Tarek Auf der Strasse <69731049+codetakki@users.noreply.github.com>
2024-09-30 11:13:43 +0200

    Changed main layout

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,22 +1,38 @@
 <template>
   <div class="align-center d-flex flex-column justify-center h-100 w-100 text-center px-2 py-4 overflow-x-hidden">
-    <div class="text-h2 font-weight-bold">Enter your adress</div>
-    <div class="my-4  my-md-8  ">
-      <v-text-field v-model="appStore.typedAdress"
-        width="300"
-        variant="outlined"
-        class="text-h3"
-        :style="{ scale: mobile ? 1 : 1.4 }"
-        label="Adress" />
-    </div>
-    <v-expand-transition>
-      <div v-if="restaurant"
+    <v-expand-transition group>
+      <div key="search-title" class="d-flex flex-column align-center">
+        <div class="text-h2 font-weight-bold">Enter your adress</div>
+        <v-chip v-if="appStore.fetchingCoordinatesAdress">Getting address
+          <template #prepend><v-progress-circular indeterminate size="20" class="ml-n1 mr-1"></v-progress-circular></template>
+        </v-chip>
+        <div class="my-4  my-md-8  ">
+          <v-text-field v-model="appStore.typedAdress"
+            width="300"
+            variant="outlined"
+            class="text-h3"
+            :style="{ scale: mobile ? 1 : 1.4 }"
+            label="Adress" />
+        </div>
+      </div>
+      <div v-if="appStore.fetchingRestaurants || appStore.fetchingAdressCoordinates"
+        width="300" key="progress">
+        <v-progress-circular indeterminate
+          :size="128"
+          :width="15"
+          class="text-h2"
+          speed="0.9">
+          🍽️
+        </v-progress-circular>
+
+      </div>
+      <div v-else-if="restaurant" key="result"
         class="d-flex flex-column align-center w-100">
         <div class="text-md-h3 text-h4 font-weight-bold">What do you think about:</div>
         <v-card v-if="restaurant"
-          :width="mobile ? '90%' : 600"
+          :width="mobile ? '90%' : 600" min-height="140"
           :style="{ scale: mobile ? 1 : 1 }"
-          class="text-left my-12 mx-2">
+          class="text-left my-4 mx-2">
           <v-card-title class="text-h5 text-md-h3 text-bold d-flex align-center">
             <div class="flex-shrink-1 text-truncate">{{ restaurant.name }}</div>
             <v-btn icon="mdi-open-in-new"
@@ -26,7 +42,15 @@
           </v-card-title>
           <v-card-text class="text-h6 text-md-h5">
             <div v-if="restaurant.amenity_type">{{ amenityMap[restaurant.amenity_type] }}</div>
-            <span v-if="restaurant.cuisine">{{ getCuisine(restaurant.cuisine || '') }}</span>
+            <div v-if="restaurant.cuisine">{{ getCuisine(restaurant.cuisine || '') }}</div>
+            <div>
+              <a v-if="restaurant.phone"
+                :href="`tel:${restaurant.phone.replace(/[^\d]/g, '')}`"
+                class="text-decoration-none">
+                ☎️ {{ restaurant.phone }}
+              </a>
+            </div>
+
             <!-- <pre>{{ restaurant }}</pre> -->
           </v-card-text>
         </v-card>
@@ -47,6 +71,8 @@
   import { cuisineMap, amenityMap } from '@/assets/cuisine-map';
   import { useDisplay } from 'vuetify';
   import { useFetch } from '@vueuse/core';
+  import emojiFromWord from "emoji-from-word";
+
   const { mobile } = useDisplay()
   const appStore = useAppStore()
   const { randomRestaurant: restaurant } = storeToRefs(appStore)
@@ -75,7 +101,7 @@
     console.log(restaurantLocation.value);
 
     var string = `${restaurant.value?.name} restaurant`
-    if (!restaurantLocation.value) return ''
+    if (!restaurantLocation.value || !restaurant.value?.longitude) return ''
     else { string += ` in ${restaurantLocation.value.address.road || restaurantLocation.value.address.house_number} ${restaurantLocation.value.address.city || restaurantLocation.value.address.municipality}` }
     return `https://www.google.com/search?q=${string.replace(' ', '+')}`
   })
